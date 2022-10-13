69c1a66eb2f737384b3c32c219428a32145e258e
salyijanos <55670326+salyijanos@users.noreply.github.com>
2022-10-13 13:08:43 +0200

    Add files via upload

---
+++ 01_Neptun/Hallgato.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Neptun
+{
+    class Hallgato
+    {
+        private Hallgato()
+        {
+            this.Kreditek = 0;
+        }
+        public Hallgato(string NeptunKod)
+            : this()
+        {
+            this.NeptunKod = NeptunKod;
+        }
+        public Hallgato(string NeptunKod, DateTime SzuletesiDatum)
+            : this(NeptunKod)
+        {
+            this.SzuletesiDatum = SzuletesiDatum;
+        }
+
+        private string nev;
+        public string Nev
+        {
+            set
+            {
+                if (value.Contains(" "))
+                    nev = value;
+                else throw new Exception("Hiba: Használja a 'Vezetéknév Családnév' formátumot!");
+            }
+            get
+            {
+                return nev;
+            }
+        }
+
+        private bool neptunkodMegadva = false;
+        private string neptunKod;
+        public string NeptunKod
+        {
+            private set
+            {
+                if (neptunkodMegadva)
+                    throw new Exception("A neptunkód már meg van adva!");
+
+                if (value.Length != 6)
+                    throw new Exception("Hibás neptunkód!");
+
+                string ABC = "0123456789QWERTZUIOPASDFGHJKLYXCVBNM";
+                value = value.ToUpper();
+                if (ABC.Substring(0, 10).Contains(value[0]))
+                    throw new Exception("Az első karakter nem lehet szám!");
+                for (int i = 0; i < value.Length; i++)
+                    if (!ABC.Contains(value[i]))
+                        throw new Exception("Tiltott karakter!");
+
+                neptunKod = value;
+                neptunkodMegadva = true;
+            }
+            get
+            {
+                return neptunKod;
+            }
+        }
+
+        private bool szuletesiDatumMegadva = false;
+        private DateTime szuletesiDatum;
+        public DateTime SzuletesiDatum
+        {
+            set
+            {
+                if (szuletesiDatumMegadva)
+                    throw new Exception("Hiba: A mező csak egyszer módosítható!");
+                if ((DateTime.Now - value).TotalDays / 365 < 17)
+                    throw new Exception("Hiba: A minimális életkor 17 év!");
+
+                szuletesiDatum = value;
+                szuletesiDatumMegadva = true;
+            }
+            get
+            {
+                return szuletesiDatum;
+            }
+        }
+
+        public int Eletkor
+        {
+            get
+            {
+                return (int)((DateTime.Now - SzuletesiDatum).TotalDays / 365);
+            }
+        }
+
+        //private Nem neme;
+        //public Nem Neme
+        //{
+        //    set { neme = value; }
+        //    get { return neme; }
+        //}
+        public Nem Neme { set; get; }
+
+        private int kreditek;
+        public int Kreditek
+        {
+            set
+            {
+                if (value >= 0 && value <= 190)
+                    kreditek = value;
+                else throw new Exception("Hiba: A kreditek száma 0 és 190 közötti egész!\n" +
+                    "A hibás input: " + value);
+            }
+            get
+            {
+                return kreditek;
+            }
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}",
+                    Nev.Length > 15 ?
+                        Nev.Substring(0, 12) + "..."
+                        : Nev,
+                    NeptunKod,
+                    SzuletesiDatum.ToString("yyyy.MM.dd"),
+                    Neme,
+                    Kreditek);
+        }
+        public string HallgatoString()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}",
+                    Nev.Length > 15 ?
+                        Nev.Substring(0, 12) + "..."
+                        : Nev,
+                    NeptunKod,
+                    SzuletesiDatum.ToString("yyyy.MM.dd"),
+                    Neme,
+                    Kreditek);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t\t{5} év",
+                    Nev.Length > 15 ?
+                        Nev.Substring(0, 12) + "..."
+                        : Nev,
+                    NeptunKod,
+                    SzuletesiDatum.ToString("yyyy.MM.dd"),
+                    Neme,
+                    Kreditek,
+                    Eletkor);
+        }
+    }
+}
---
+++ 01_Neptun/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Neptun
+{
+    class Program
+    {
+        static List<Hallgato> Hallgatok = new List<Hallgato>();
+        static bool Kilepes = false;
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("*** Neptun ***\n");
+            Console.WriteLine("1 - Új hallgató");
+            Console.WriteLine("2 - Hallgatók listája");
+            Console.WriteLine("3 - Törlés");
+            Console.WriteLine("Esc - Kilépés");
+        }
+        static void MenuKezeles()
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    HallgatoRegisztralas();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    HallgatokListaja();
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    break;
+                case ConsoleKey.Escape:
+                    Kilepes = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void HallgatoRegisztralas()
+        {
+            Console.Clear();
+            Console.WriteLine("*** Új hallgató ***\n");
+
+            Console.Write("Neptun kód: ");
+            Hallgato hallgato = new Hallgato(Console.ReadLine());
+
+            Console.Write("Név: ");
+            hallgato.Nev = Console.ReadLine();            
+            Console.WriteLine("Születési dátum: ");
+            int ev, honap, nap;
+            Console.Write("Év: ");
+            ev = int.Parse(Console.ReadLine());
+            Console.Write("Hónap: ");
+            honap = int.Parse(Console.ReadLine());
+            Console.Write("Nap: ");
+            nap = int.Parse(Console.ReadLine());
+            hallgato.SzuletesiDatum =
+                new DateTime(ev, honap, nap);
+            Console.Write("Neme: ");
+            hallgato.Neme = (Nem)Enum.Parse(typeof(Nem), Console.ReadLine(), true);
+            Console.Write("Kreditek száma: ");
+            hallgato.Kreditek = int.Parse(Console.ReadLine());
+
+            Hallgatok.Add(hallgato);
+            Console.WriteLine("\nSikeres mentés!");
+            Console.ReadLine();
+        }
+        static void HallgatokListaja()
+        {
+            Console.Clear();
+            Console.WriteLine("*** Hallgatók ***\n");
+            Console.WriteLine("Név\t\tNeptKod\tSzulDatum\tNeme\tKreditek\tÉletkor");
+            foreach (Hallgato hallgato in Hallgatok)
+            {
+                //hallgato.Kiir();
+                //Console.WriteLine(hallgato.HallgatoString());
+                Console.WriteLine(hallgato);
+            }
+
+            Console.WriteLine("\nNyomjon Enter-t a folytatáshoz!");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //Tantargy MagProg2 = new Tantargy();
+            //MagProg2.
+
+            Hallgato hallgato = new Hallgato("ABC775");
+            hallgato.Nev = "Nagy Géza";
+            hallgato.SzuletesiDatum =
+                new DateTime(1991, 8, 3);
+            hallgato.Neme = Nem.FERFI;
+            //hallgato.Kreditek = 180;
+            Hallgatok.Add(hallgato);
+
+            Hallgato hallgato2 = new Hallgato("DFE885",
+                new DateTime(1969, 9, 27));
+            hallgato2.Nev = "Kiss Péter";
+            hallgato2.Neme = Nem.FERFI;
+            hallgato2.Kreditek = 181;
+            Hallgatok.Add(hallgato2);
+
+            Hallgato hallgato3 = new Hallgato("GHJ762");
+            hallgato3.Nev = "Horváth Gizi";
+            hallgato3.SzuletesiDatum =
+                new DateTime(1981, 9, 5);
+            hallgato3.Neme = Nem.NO;
+            hallgato3.Kreditek = 20;
+            Hallgatok.Add(hallgato3);
+
+            do
+            {
+                Menu();
+                MenuKezeles();
+            } while (!Kilepes);
+        }
+    }
+}
